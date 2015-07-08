ad85d9f000a66600320c5035e5d682ae9eb5c398
Juha Rantanen <juha@codercoded.com>
2015-07-08 19:30:22 +0300

    Better version

--- AdalightFastled.ino
+++ AdalightFastled.ino
@@ -1,10 +1,8 @@
 #include <FastLED.h>
 
 #define NUM_LEDS 156
-#define DATA_PIN 6
+#define DATA_PIN 2
 #define SERIALRATE 500000
-#define BLACK_THRESHOLD 10
-#define BLACK CRGB(0,0,0)
 #define CALIBRATION_TEMPERATURE TypicalLEDStrip  // Color correction
 #define MAX_BRIGHTNESS 255 // 0-255
 
@@ -12,7 +10,6 @@
 uint8_t prefix[] = {'A', 'd', 'a'}, hi, lo, chk, i;
 
 CRGB leds[NUM_LEDS];
-CRGB incomes[NUM_LEDS];
 
 void setup()
 {
@@ -21,15 +18,6 @@ void setup()
 
   FastLED.setTemperature( CALIBRATION_TEMPERATURE );
   FastLED.setBrightness( MAX_BRIGHTNESS );
-
-  // initial RGB flash
-  // LEDS.showColor(CRGB(255, 0, 0));
-  // delay(500);
-  // LEDS.showColor(CRGB(0, 255, 0));
-  // delay(500);
-  // LEDS.showColor(CRGB(0, 0, 255));
-  // delay(500);
-  // LEDS.showColor(CRGB(0, 0, 0));
 
   Serial.begin(SERIALRATE);
   Serial.print("Ada\n"); // Send "Magic Word" string to host
@@ -61,21 +49,8 @@ void loop() {
     goto waitLoop;
   }
 
-  Serial.readBytes((char*)incomes, NUM_LEDS*3);
+  Serial.readBytes((char*)leds, NUM_LEDS*3);
 
-  for(i = 0; i < NUM_LEDS; i++) {
-    CRGB *led = leds+i; 
-    CRGB *income = incomes+i; 
-
-    if( (*income).getAverageLight() < BLACK_THRESHOLD ) {
-      *led = BLACK; 
-    } else {
-      //(*led).nscale8_video(127); // 50% from previous
-      //(*income).nscale8_video(127); // 50% from current
-      //(*led) += (*income);
-      (*led) = (*income);
-    }
-  }
   // shows new values
  FastLED.show();
 }
